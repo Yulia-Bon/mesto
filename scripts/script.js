b5b985b984d69f6a0296a37261c8a8b93d107e57
Yulia Bonohova <ybonohova@gmail.com>
2021-11-10 20:23:54 +0300

    add arr cards/unit_5

--- scripts/script.js
+++ scripts/script.js
@@ -40,3 +40,34 @@ function popupClose() {
 formElement.addEventListener("submit", formSubmitHandler);
 closeButton.addEventListener("click", popupClose);
 editButton.addEventListener("click", popupOpen);
+
+
+
+
+//массив для 5ой работы
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
