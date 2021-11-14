21a11db0ed9ab69bbd6641aaea7292921a71f82c
Yulia Bonohova <ybonohova@gmail.com>
2021-11-14 16:41:17 +0300

    add like

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,32 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+
 // кнопки Закрыть, Редактировать
 let closeButton = document.querySelector(".popup__close");
 let editButton = document.querySelector(".profile__edit");
@@ -44,30 +73,65 @@ editButton.addEventListener("click", popupOpen);
 
 
 
-//массив для 5ой работы
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+
+
+
+// кнопки Закрыть, Редактировать
+//let closeButton = document.querySelector(".popup__close");
+let addButton = document.querySelector(".profile__add-button");
+//  попап
+let popupPhotos = document.querySelector(".popup-photos");
+//  формa
+let formElementPhotos = document.querySelector(".popup-photos__container"); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let cardInput = document.querySelector(".popup-photos__input_type_card-name"); // Воспользуйтесь инструментом .querySelector()
+let srcInput = document.querySelector(".popup-photos__input_type_card-src"); // Воспользуйтесь инструментом .querySelector()
+let cardName = document.querySelector(".card__caption");
+let cardPhoto = document.querySelector(".card__photo");
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandlerPhoto(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+  // Получите значение полей jobInput и nameInput из свойства value
+  cardName.textContent = cardInput.value;
+  cardPhoto.textContent = srcInput.value;
+
+  // Закрываем попап
+  popupClose();
+}
+
+function popupOpenPhoto() {
+  popup-photos.classList.add("popup-photos_opened");
+
+ // cardInput.value = cardName.textContent;
+ // srcInput.value = cardPhoto.textContent;
+}
+
+function popupClosePhoto() {
+  popup-photos.classList.remove("popup-photos_opened");
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElementPhotos.addEventListener("submit", formSubmitHandlerPhoto);
+closeButton.addEventListener("click", popupClose);
+addButton.addEventListener("click", popupOpenPhoto);
+
+
+
+
+
+//Лайк
+const cardList = document.querySelector('.photo-grid');
+function likePhoto(evt) {
+  if (!evt.target.matches('.photo-grid__like')) {
+    return;
   }
-];
+  else {
+    evt.target.classList.toggle('photo-grid__like_active');
+  }
+}
+
+cardList.addEventListener('click', likePhoto);
