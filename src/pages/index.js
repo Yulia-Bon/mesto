993adb3374714dd89033c0699f63728a7600f6d4
Yulia Bonohova <ybonohova@gmail.com>
2021-12-30 22:00:36 +0300

    webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import '../pages/index.css';
 import {
   initialCards,
   popupFull,
@@ -11,14 +12,14 @@ import {
   popupUser,
   validationSettings,
   cardTemplate,
-} from "./initCards.js";
+} from "../components/initCards.js";
 
-import Card from "./Card.js";
-import { FormValidator } from "./FormValidator.js";
-import { PopupWithImage } from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
-import Section from "./Section.js";
+import Card from "../components/Card.js";
+import { FormValidator } from "../components/FormValidator.js";
+import { PopupWithImage } from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
+import Section from "../components/Section.js";
 
 //ОБЬЕКТЫ(ЭКЗЕМПЛЯРЫ) КЛАССА ВАЛИДАЦИИ
 const formProfileValidation = new FormValidator(
